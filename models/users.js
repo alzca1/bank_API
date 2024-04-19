45a0e1b8779be5f38fc583e7737f222c4686dd79
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-04-19 18:03:04 +0200

    añadida columna "id" al modelo Users

--- models/users.js
+++ models/users.js
@@ -4,6 +4,11 @@ module.exports = (sequelize) => {
   return sequelize.define(
     "Users",
     {
+      id: {
+        type: Datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
       name: {
         type: Datatypes.STRING(50),
         allowNull: false,
