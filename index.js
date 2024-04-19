7ab187949a388023436d72de4fb5b68ec8a6d550
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-04-19 16:43:39 +0200

    creación inicial de rutas GET, POST y PUT para la aplicación

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ app.get("/", (req, res) => {
   res.send("main route works well");
 });
 
+app.get("/transactions", (req, res) => {
+  res.send("transactions route works well");
+});
+
+app.post("/withdrawals", (req, res) => {
+  res.send("withdrawals route works well");
+});
+
+app.post("/deposits", (req, res) => {
+  res.send("deposits route works well");
+});
+
+app.post("/transfers", (req, res) => {
+  res.send("transfers route works well");
+});
+
+app.post("/activations", (req, res) => {
+  res.send("activations route works well");
+});
+
+app.put("/pinchanges", (req, res) => {
+  res.send("balances route works well");
+});
+
 app.listen(PORT, () => {
   console.log("Server started on port 3000");
 });
