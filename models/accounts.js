d57273d39245c85e38ef92fb2736507bf2bc1650
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-04-20 14:19:57 +0200

    añadida columna accountNumber a modelo "Account"

--- models/accounts.js
+++ models/accounts.js
@@ -2,6 +2,10 @@ const { DataTypes, Sequelize } = require("sequelize");
 
 module.exports = (sequelize) => {
   return sequelize.define("Accounts", {
+    accountNumber: {
+      type: DataTypes.STRING(16),
+      allowNull: false,
+    },
     balance: {
       type: DataTypes.FLOAT,
       defaultValue: 0,
