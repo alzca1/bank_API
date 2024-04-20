e2fad6786016fac504ec809dd007423450657d0b
Álvaro Zájara <alvaro.zajara@eviden.com>
2024-04-20 14:21:13 +0200

    actualizada lógica para acomodar nuevos modelos "Bank" y "Cashpoint" en la creación y asociación de tablas en base de datos.

--- db/database.js
+++ db/database.js
@@ -32,33 +32,57 @@ db.users = require("../models/users")(sequelize, Sequelize);
 db.accounts = require("../models/accounts")(sequelize, Sequelize);
 db.cards = require("../models/cards")(sequelize, Sequelize);
 db.transactions = require("../models/transactions")(sequelize, Sequelize);
+db.banks = require("../models/banks")(sequelize, Sequelize);
+db.cashpoints = require("../models/cashpoints")(sequelize, Sequelize);
 
 db.users.hasMany(db.accounts, {
   as: "account",
   foreignKey: "userId",
-  onDelete: "CASCADE",
-  onUpdate: "CASCADE",
 });
 
 db.accounts.hasMany(db.cards, {
   as: "card",
   foreignKey: "accountId",
-  onDelete: "CASCADE",
-  onUpdate: "CASCADE",
 });
 
 db.accounts.hasMany(db.transactions, {
   as: "transaction",
   foreignKey: "accountId",
-  onDelete: "CASCADE",
-  onUpdate: "CASCADE",
+});
+
+db.accounts.hasMany(db.users, {
+  as: "user",
+  foreignKey: "accountId",
 });
 
 db.cards.hasMany(db.transactions, {
   as: "transaction",
   foreignKey: "cardId",
-  onDelete: "CASCADE",
-  onUpdate: "CASCADE",
+});
+
+db.transactions.belongsTo(db.cards, {
+  as: "card",
+  foreignKey: "cardId",
+});
+
+db.transactions.belongsTo(db.accounts, {
+  as: "account",
+  foreignKey: "accountId",
+});
+
+db.cards.belongsTo(db.accounts, {
+  as: "account",
+  foreignKey: "accountId",
+});
+
+db.banks.hasMany(db.cashpoints, {
+  as: "cashpoint",
+  foreignKey: "bankId",
+});
+
+db.cashpoints.belongsTo(db.banks, {
+  as: "bank",
+  foreignKey: "bankId",
 });
 
 module.exports = { db };
